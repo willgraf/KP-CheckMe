9e4e8152c4f0d1a0f0fc1777b3d5c82da7c6a1c6
Ben Krawitz <benkraw@umich.edu>
2015-06-21 13:11:06 -0400

    alert dlt

--- js/login2.js
+++ js/login2.js
@@ -1,7 +1,6 @@
  $("#login-button").click(function(event){
 		 event.preventDefault();
 	 
-	 alert("Note: You may be contacted by Kaiser, if your symptoms are severe!"); 
 	 $('form').fadeOut(500);
 	 $('.wrapper').addClass('form-success');
 	 window.location.href = "http://kpcheckme.azurewebsites.net/dashboard.html";
