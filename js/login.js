2724a08adcbcd76e30020a2b4c958cf61325e44f
Ben Krawitz <benkraw@umich.edu>
2015-06-21 12:52:40 -0400

    alerttts

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@
 	 
 	 $('form').fadeOut(500);
 	 $('.wrapper').addClass('form-success');
-
+	 alert("Note: You may be contacted by Kaiser, if your symptoms are severe!")
 	 window.location.href = "http://kpcheckme.azurewebsites.net/results.html";
 
 });
