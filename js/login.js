e0e62df23ed1f96a9483e18938f64cc3bdb16cfd
Ben Krawitz <benkraw@umich.edu>
2015-06-21 13:07:54 -0400

    alert?

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
  $("#login-button").click(function(event){
 		 event.preventDefault();
-	alert("Note: You may be contacted by Kaiser, if your symptoms are severe!")
+	 
+	 alert("Note: You may be contacted by Kaiser, if your symptoms are severe!");
 	 $('form').fadeOut(500);
 	 $('.wrapper').addClass('form-success');
 	 window.location.href = "http://kpcheckme.azurewebsites.net/results.html";
